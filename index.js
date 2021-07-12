59f05f44d48822ea329c0ea8a525950916693094
Mauro <progettazione@studiogb.org>
2021-07-12 15:54:35 +0200

    inseriti commenti

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ fs.readFile('testo.txt', 'utf8', (error,data) => {
 			console.log(data)
 		}
 })
+
+
+// Tutorial: https://nodejs.dev/learn
+// GIT :https://dont-be-afraid-to-commit.readthedocs.io/en/latest/git/commandlinegit.html
+// Per stage vedere QUI: https://www.nobledesktop.com/learn/git/stage-commit-files
