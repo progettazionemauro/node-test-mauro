81a4d9a793bd8b0c9761a762479da80967d28184
progettazionemauro <progettazionemauro>
2021-07-13 11:29:27 +0200

    terzo

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ fs.readFile('testo.txt', 'utf8', (error,data) => {
 // Install the latest version of Git: https://adrianba.net/2021/03/09/ubuntu-latest-git/
 // Insert you username: https://docs.github.com/en/get-started/getting-started-with-git/setting-your-username-in-git
 // Add project Locally: https://docs.github.com/en/github/importing-your-projects-to-github/importing-source-code-to-github/adding-an-existing-project-to-github-using-the-command-line
+// Spiegazione dei termini Git https://medium.com/mindorks/what-is-git-commit-push-pull-log-aliases-fetch-config-clone-56bc52a3601c
+
+
+
+// Flusso Git
+// 1) git add .
+// 2) git comit -m "...."
+// 3) git remote add origin  <REMOTE_URL> 
+// 4) git remote -v (Verifies the new remote URL)
+// 5) $ git push origin main (Pushes the changes in your local repository up to the remote repository you specified as the origin)
